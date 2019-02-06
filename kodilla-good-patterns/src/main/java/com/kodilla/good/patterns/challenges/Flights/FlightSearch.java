60da62edf4ecdf0ed8b881aa9eee24ba3a3c23b1
majkel3321 <majkel3321@gmail.com>
2019-02-06 14:47:10 +0100

    BeanForumUser

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/Flights/FlightSearch.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/Flights/FlightSearch.java
@@ -10,11 +10,11 @@ public class FlightSearch {
     private List<Flight> flightList;
 
 
-    public FlightSearch(List<Flight> flightList){
+    public FlightSearch(List<Flight> flightList) {
         this.flightList = flightList;
     }
 
-    public void searchFrom(String city){
+    public void searchFrom(String city) {
 
         flightList.stream()
                 .filter(flight -> flight.getFrom().equals(city))
@@ -22,17 +22,17 @@ public class FlightSearch {
     }
 
 
-    public void searchTo(String city){
+    public void searchTo(String city) {
 
         flightList.stream()
                 .filter(flight -> flight.getTo().equals(city))
                 .forEach(System.out::println);
     }
 
-    public void searchFromTo(String from, String to){
+    public void searchFromTo(String from, String to) {
 
 
-       List<Flight> startA = flightList.stream()
+       /*List<Flight> startA = flightList.stream()
                .filter(flight -> flight.getFrom().equals(from))
                .collect(Collectors.toList());
 
@@ -53,12 +53,28 @@ public class FlightSearch {
        flightList.stream()
                .filter(flight -> flight.getFrom().equals(startB))
                .filter(flight -> flight.getTo().equals(to))
-               .forEach(System.out::println);
+               .forEach(System.out::println);*/
 
 
-             }
+        List<String> toB = flightList.stream()
+                .filter(flight -> flight.getFrom().equals(from))
+                .map(flight -> flight.getTo())
+                .collect(Collectors.toList());
 
-      }
+
+
+
+
+    }
+}
+
+
+
+
+
+
+
+
 
 
 
