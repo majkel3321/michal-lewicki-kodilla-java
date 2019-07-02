8e932480d74e8e21f036d881f940db963593d49e
majkel3321 <majkel3321@gmail.com>
2019-07-02 14:19:55 +0200

    Laptops on eBay

--- kodilla-testing2/src/main/java/com/kodilla/testing2/ebay/EBayTestingApp.java
+++ kodilla-testing2/src/main/java/com/kodilla/testing2/ebay/EBayTestingApp.java
@@ -1,4 +1,19 @@
 package com.kodilla.testing2.ebay;
 
+import com.kodilla.testing2.config.WebDriverConfig;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
 public class EBayTestingApp {
+    public static final String SEARCHFIELD = "//*[@id=\"gh-ac\"]";
+
+    public static void main(String[] args) {
+        WebDriver webDriver = WebDriverConfig.getDriver(WebDriverConfig.CHROME);
+        webDriver.get("https://ebay.com");
+
+        WebElement searchField = webDriver.findElement(By.xpath(SEARCHFIELD));
+        searchField.sendKeys("Laptop");
+        searchField.submit();
+    }
 }
